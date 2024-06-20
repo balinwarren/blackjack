6e67f78a9aadf3003035b128fc0e4c5f1903f58f
Balin Warren <balin.warren@gmail.com>
2024-06-19 17:58:21 -0700

    Added play command

---
+++ cmd/play.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/balinwarren/blackjack/internal/ascii"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(playCmd)
+}
+
+var playCmd = &cobra.Command{
+	Use:   "play",
+	Short: "Start a game of blackjack.",
+	Long:  `Beat the dealer living in your terminal. Play a game of blackjack.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(ascii.GetTitle())
+	},
+}
--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -10,18 +9,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "blackjack",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Beat the dealer right inside your terminal.",
+	Long: `Blackjack is a CLI game that allows the blackjack table
+to live inside your terminal. This application lets you
+play full ascii art blackjack with a single command.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -47,5 +41,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
