37b171884e5c2c1f9f25473a871909f84d99c8f5
Balin Warren <balin.warren@gmail.com>
2024-06-22 20:17:41 -0700

    Added draw card function

--- internal/game/game.go
+++ internal/game/game.go
@@ -6,7 +6,12 @@ import (
 	"github.com/balinwarren/blackjack/internal/ascii"
 )
 
+var deck []ascii.Card = ascii.GetDeck()
+
 func GenerateHand() []ascii.Card {
-	deck := ascii.GetDeck()
 	return []ascii.Card{deck[rand.IntN(14)], deck[rand.IntN(14)]}
+}
+
+func DrawCard() ascii.Card {
+	return deck[rand.IntN(14)]
 }
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ Copyright © 2024 Balin Warren
 */
 package main
 
-import "github.com/balinwarren/blackjack/cmd"
+import (
+	"github.com/balinwarren/blackjack/cmd"
+)
 
 func main() {
 	cmd.Execute()
